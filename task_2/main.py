dcbba3a6148638ada691e8d33bc85d5629aa3a6b
devcb13d <devcb@pg.cusat.ac.in>
2024-02-06 06:55:24 +0530

    fixed reuse of arr1

--- task_2/main.py
+++ task_2/main.py
@@ -1,18 +1,22 @@
 import numpy as np 
 import time
 
+arr_shape = [1000,1000]
+arr_dtype = np.float64
+
 
 start = time.time()
-arr = np.random.rand(1000,1000)
+arr1 = np.random.rand(*arr_shape).astype(arr_dtype)
+
 
 print("array created in: ", time.time()-start)
 
 # converting to bytes
+bytearray = arr1.tobytes()
 
-bytearray = arr.tobytes()
 
 # converting back to array
+arr2 = np.frombuffer(bytearray, dtype=arr_dtype).reshape(arr_shape)
 
-arr2 = np.frombuffer(bytearray, dtype=arr.dtype).reshape(arr.shape)
+print(arr1 == arr2)
 
-print(arr2 == arr)
